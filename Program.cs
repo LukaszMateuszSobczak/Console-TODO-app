70cdfc8fa1013452fa952fe669f9c0c3be4a8fbe
Lukasz <sobczak.lukasz.mateusz@outlook.com>
2025-09-23 19:51:51 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TodoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Waht do you want to do?");
+            //Console.WriteLine("[S]ee ale TODOs");
+            //Console.WriteLine("[A]dd a TODO");
+            //Console.WriteLine("[R]emove a TODO");
+            //Console.WriteLine("[E]xit");
+
+            //string userChoice = Console.ReadLine();
+
+            //if (userChoice == "S")
+            //{
+            //    PrintSelectetOption("See all TODOs");
+            //}
+            //else if (userChoice == "A")
+            //{
+            //    PrintSelectetOption("Add a TODO");
+            //}
+            //else if (userChoice == "R")
+            //{
+            //    PrintSelectetOption("Remove TODO");
+            //}
+            //else if (userChoice == "E")
+            //{
+            //    PrintSelectetOption("Exit program");
+            //}
+
+            Console.WriteLine("Input a number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+
+
+        }
+
+        static void PrintSelectetOption(string selectedOption)
+        {
+            Console.WriteLine("Selected option: " + selectedOption);
+        }
+    }
+}
